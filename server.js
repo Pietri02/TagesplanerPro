6717ddf7057f3d88368c2abfc481101249d28617
Pietri02 <6pm.pietri@gmail.com>
2025-10-25 01:14:28 +0200

    Render Deployment Konfiguration

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const SECRET_KEY = 'dein-super-geheimer-schluessel-aendere-mich';
 
 // Middleware
